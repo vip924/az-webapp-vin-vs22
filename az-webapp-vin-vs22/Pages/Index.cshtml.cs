1cfc910613132782cda509882639d7ab060cca1d
Vinu Philip <Vinu_Philip@ajg.com>
2025-10-13 15:55:53 +0530

    Added a new configuration message

--- az-webapp-vin-vs22/Pages/Index.cshtml.cs
+++ az-webapp-vin-vs22/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace az_webapp_vin_vs22.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"];
         }
     }
 }
